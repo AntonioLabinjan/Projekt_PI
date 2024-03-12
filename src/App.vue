7a8517f5f3fddd01de54e71cc581e8a2a04ae62a
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-12 18:45:17 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view /> <!-- Ovdje će se dinamički učitavati komponente prema rutama iz main.js -->
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style>
