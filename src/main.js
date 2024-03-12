c568114980e2c57d8847d119f6ea61e61d62c00d
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-12 19:46:25 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/HomePage.vue';
+import ImageGallery from './components/ImageGallery.vue';
 const routes = [
-  { path: '/', component: HomePage },  ];
+  { path: '/', component: HomePage },
+  { path: '/image-gallery', component: ImageGallery },  
+];
 
 
 const router = createRouter({
@@ -14,5 +17,6 @@ const router = createRouter({
 
 const app = createApp(App);
 app.component('HomePage', HomePage);
+app.component('ImageGallery', ImageGallery);
 app.use(router);
 app.mount('#app');
