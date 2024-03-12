b1fb1f0b271f6e22bec829a05a3a3a78bd51b85a
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-12 19:36:24 +0100

    Update and rename HelloWorld.vue to HomePage.vue
    Kreiranje homepagea i pokušaj rutiranja na galeriju slika

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <h1>Welcome to Vue Trainer</h1>
+<hr>
+      <h4>Your personal training application</h4>
+      <p>Add your exercises, meals, sleep, water intake, calculate your BMI, and track your progress!</p>
+    </div>
+    <nav>
+      <ul class="navbar">
+        <li><button @click="goToImageGallery">Image Gallery</button></li>
+      </ul>
+    </nav>
+    <div class="about-button">
+      <button @click="goToAbout">About</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToImageGallery() {
+      this.$router.push({ path: '/image-gallery' });
+    },
+  }
+};
+</script>
+
+<style scoped>
+</style>
