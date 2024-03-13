d59d93538931043dfb021f93e884cff2f28d265f
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:38:13 +0100

    Create DefaultMeals.vue

---
+++ src/components/DefaultMeals.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovo će biti ista stvar kao DefaultEcercises.vue, ali za obroke-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
