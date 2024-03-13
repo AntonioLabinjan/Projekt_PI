59f7a6bc183ca2d46d0563561cae1604ca41bbe3
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:36:46 +0100

    Create DefaultExercises.vue

---
+++ src/components/DefaultExercises.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovdje ću ubaciti neke hardkodirane "kartice" s vježbama. Također, omogućit ću CRUD operacije nad njima -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
