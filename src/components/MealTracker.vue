7c04af2691771d1ab4f947f4934a5f181f3d817f
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:50:46 +0100

    Create MealTracker.vue
    Komponenta za obroke

---
+++ src/components/MealTracker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovdje ćemo imati CRUD operacije nad obrocima, neke automatske izračune, možda grafove, filter itd. -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
