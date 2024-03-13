9670a851a94baca14f44c70c7afb8c3eb48d01cf
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:56:32 +0100

    Create WaterIntake.vue
    Kreirao komponentu za praćenje unosa tekućine

---
+++ src/components/WaterIntake.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- U ovoj komponenti ćemo pratiti unos tekućine...još moram malo razmisliti na koju bi foru to radilo -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
