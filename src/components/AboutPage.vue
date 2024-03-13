c62b82cfe501a504a1fa6fd8da56f695b7a4c30b
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:16:27 +0100

    Create AboutPage.vue
    Iskopirao sam si par identičnih praznih templateova za komponente koje planiram ubaciti, pa ću si ih samo iskopirati i mijenjati im imena. Onda ću ih naknadno uređivati i dodavati funkcionalnosti

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Vaš sadržaj komponente ide ovdje -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
