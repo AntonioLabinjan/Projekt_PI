0ba83c8ca05aa6fc2060ad7c19c92f6d78edce24
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:40:51 +0100

    Create LogIn.vue

---
+++ src/components/LogIn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovo će biti komponenta za login...trebat će mi Firebase...još moram skužit kako to točno funkcionira -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
