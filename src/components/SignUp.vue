45a74472e437c21fb46e6a5c404a1203beb7f76a
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:48:41 +0100

    Create SignUp.vue
    Dodana prazna komponenta u kojoj ću napraviti signup

---
+++ src/components/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovo će biti komponenta za signup... -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
