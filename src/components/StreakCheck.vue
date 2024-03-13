1438660ca277832e4460e60981f0b1e98ae125d8
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-13 18:54:57 +0100

    Create StreakCheck.vue

---
+++ src/components/StreakCheck.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="empty-component">
+    <!-- Ovdje ćemo moći dodavati dane kada smo trenirali, pratit će se streak dana, kad postignemo određeni streak, dobit ćemo neku "nagradu" (najvjerojatnije ću ubaciti neku grafičku ikonicu koja će nam se prikazati, ili tako nešto) -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyComponent',
+  // Dodajte potrebne podatke, metode, computed properties itd.
+}
+</script>
+
+<style scoped>
+.empty-component {
+  /* Stilovi za praznu komponentu */
+}
+</style>
