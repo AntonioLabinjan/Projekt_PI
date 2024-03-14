8510ce22dc5b4d40f410119ffef781c70b5f507f
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-14 08:39:34 +0100

    Update HomePage.vue
    Dodane metode za rad rutera koje povezuju homepage sa svim ostalim rutama

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -23,6 +23,33 @@ export default {
     goToImageGallery() {
       this.$router.push({ path: '/image-gallery' });
     },
+    goToVueTrainer() {
+      this.$router.push({ path: '/vue-trainer' });
+    },
+    goToMealTracker() {
+      this.$router.push({ path: '/meal-tracker' });
+    },
+    goToLogIn() {
+      this.$router.push({ path: '/login' });
+    },
+    goToSignUp() {
+      this.$router.push({ path: '/sign-up' });
+    },
+    goToSleepTracker() {
+      this.$router.push({ path: '/sleep-tracker' });
+    },
+    goToWaterIntake() {
+      this.$router.push({ path: '/water-intake' });
+    },
+    goToBMI() {
+      this.$router.push({ path: '/BMI-calculator' });
+    },
+    goToAbout() {
+      this.$router.push({ path: '/about' });
+    },
+    goToStreak() {
+      this.$router.push({ path: '/streak' });
+    },
   }
 };
 </script>
