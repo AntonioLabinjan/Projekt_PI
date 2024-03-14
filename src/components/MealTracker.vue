bcd56dd7dd1c8c9c194f61b727ab18458822fd56
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-14 14:15:19 +0100

    Update MealTracker.vue
    Dodano rutiranje

--- src/components/MealTracker.vue
+++ src/components/MealTracker.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="empty-component">
-    <!-- Ovdje ćemo imati CRUD operacije nad obrocima, neke automatske izračune, možda grafove, filter itd. -->
+  <div>
+    <h1>Meal Tracker</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'EmptyComponent',
-  // Dodajte potrebne podatke, metode, computed properties itd.
-}
+   
+    goBackHome() {
+      this.$router.push({ path: '/' });
+    },
+    goToVueTrainer() {
+      this.$router.push({ path: '/vue-trainer' });
+    },
+    goToImageGallery() {
+      this.$router.push({ path: '/image-gallery' });
+    },
+    goToSleepTracker() {
+      this.$router.push({ path: '/sleep-tracker' });
+    },
+    goToWaterIntakeTracker() {
+      this.$router.push({ path: '/water-intake' });
+    },
+    goToBMI() {
+      this.$router.push({ path: '/BMI-calculator' });
+    },
+    goToStreak() {
+      this.$router.push({path: '/streak'});
+    },
+  },
+};
 </script>
 
 <style scoped>
-.empty-component {
-  /* Stilovi za praznu komponentu */
-}
+
 </style>
