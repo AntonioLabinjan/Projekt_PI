fd3befae97d55728bfcbae334c859968eea4b060
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-14 14:25:09 +0100

    Update SleepTracker.vue
    Dodano rutiranje

--- src/components/SleepTracker.vue
+++ src/components/SleepTracker.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="empty-component">
-    <!-- Ovdje ćemo pratiti san i njegovu kvalitetu. -->
-  </div>
+  <div>
+    <h1>Sleep Tracker</h1>
+  </div>  
 </template>
 
 <script>
 export default {
-  name: 'EmptyComponent',
-  // Dodajte potrebne podatke, metode, computed properties itd.
-}
+  data() {
+    },
+  },
+  methods: {
+    
+    goBackHome() {
+      this.$router.push({ path: '/' });
+    },
+    goToImageGallery() {
+      this.$router.push({ path: '/image-gallery' });
+    },
+    goToTrainingApp() {
+      this.$router.push({ path: '/vue-trainer' });
+    },
+    goToMealTracker() {
+      this.$router.push({ path: '/meal-tracker' });
+    },
+    goToWaterIntake() {
+      this.$router.push({ path: '/water-intake' });
+    },
+    goToBMI() {
+      this.$router.push({ path: '/BMI-calculator' });
+    },
+    goToStreak() {
+      this.$router.push({ path: '/streak' });
+    },
+  },
+};
 </script>
 
 <style scoped>
-.empty-component {
-  /* Stilovi za praznu komponentu */
-}
+
 </style>
