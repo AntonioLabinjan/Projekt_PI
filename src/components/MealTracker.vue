484bc2144c2015c711df2694fc484cc7c1439c95
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-15 15:42:16 +0100

    Update MealTracker.vue
    Dodao navbar

--- src/components/MealTracker.vue
+++ src/components/MealTracker.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Meal Tracker</h1>
+    <div>
+      <nav class="navbar">
+        <ul class="navbar-nav">
+          <li><button @click="goToVueTrainer" class="btn-secondary">Go to Training app</button></li>
+          <li><button @click="goToImageGallery" class="btn-secondary">Go to Image Gallery</button></li>
+          <li><button @click="goToSleepTracker" class="btn-secondary">Go to Sleep Tracker</button></li>
+          <li><button @click="goToWaterIntakeTracker" class="btn-secondary">Go to Water Intake Tracker</button></li>
+          <li><button @click="goToBMI" class="btn-secondary">Go to BMI Calculator</button></li>
+          <li><button @click="goToStreak" class="btn-secondary">Go to Streak Tracker</button></li>
+          <li><button @click="goBackHome" class="btn btn-secondary">Go back home</button></li>
+        </ul>
+      </nav>
+    </div>
+    <hr>
   </div>
 </template>
 
