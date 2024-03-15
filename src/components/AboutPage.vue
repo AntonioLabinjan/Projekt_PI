3dcff83d874bde7cbf12396d17cb3c0d1bf670d2
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-15 10:43:43 +0100

    Update AboutPage.vue
    Dodao tipke za rutiranje koje pozivaju router.push na određene rute (plan za vikend iskonfigurirati ih na svim komponentama ako stignem)

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,12 @@
 <template>
     <h2>About Vue Trainer</h2>
     <hr>
+    </div>
+    <button @click="goToExerciseSuggestions">Go to Exercise suggestions</button>
+    <button @click="goToMealSuggestions">Go to Meal suggestions</button>
+    <hr>
+    </div>
+    <button @click="goBackHome">Go Back Home</button>
 </template>
 
 <script>
