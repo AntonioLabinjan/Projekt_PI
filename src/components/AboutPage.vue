fb1d3ab59a81a5cc6ac372a2f89f4f6926f8be8b
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-17 22:54:54 +0100

    Update AboutPage.vue
    Dodao sam sekciju "o autoru"

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -6,6 +6,20 @@
     <button @click="goToMealSuggestions">Go to Meal suggestions</button>
     <hr>
     </div>
+    <div class="author-info">
+      <h3 class="feature-title" style="color: blue;">About the Author</h3>
+      <p><b>Name:</b> Antonio Labinjan</p>
+      <p><b>Occupation:</b> 2nd-year student of Computer Science at the Faculty of Informatics in Pula</p>
+      <p><b>Mentor:</b> Nikola Tanković</p>
+      <p><b>Course:</b> Software Engineering</p>
+      <p><b>Academic Year:</b> 2023/24</p>
+      <p><b>GitHub:</b> <a href="https://github.com/AntonioLabinjan" target="_blank">github.com/AntonioLabinjan</a></p>
+      <p><b>Contact:</b></p>
+      <p>   <b> Personal email:</b> alabinjan6@gmail.com</p>
+      <p>   <b> Official email:</b> alabinjan@unipu.hr</p>
+      <p>   <b> Phone:</b> 0919135146</p>
+    </div>
+
     <button @click="goBackHome">Go Back Home</button>
 </template>
 
