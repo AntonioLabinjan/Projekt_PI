4b29c4977163e4b66b07038f55409e4478841485
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-19 18:37:31 +0100

    Update SleepTracker.vue
    Dodao crud za snove

--- src/components/SleepTracker.vue
+++ src/components/SleepTracker.vue
@@ -13,6 +13,38 @@
       </ul> 
     </nav>
   </div>
+  <hr>
+    <form v-if="!editIndex" @submit.prevent="addOrEditSleepEntry" class="sleep-input-section">
+      <h3 v-if="!editEntry">Sleep Entry</h3>
+      <h3 v-else>Edit Sleep Entry</h3>
+      <label for="date">Date:</label>
+      <input v-model="newSleepEntry.date" type="date" id="date" required />
+
+      <label for="startTime">Start Time:</label>
+      <input v-model="newSleepEntry.startTime" type="time" id="startTime" required />
+
+      <label for="wakeTime">Wake Time:</label>
+      <input v-model="newSleepEntry.wakeTime" type="time" id="wakeTime" required />
+
+      <label for="quality">Quality (1-10):</label>
+      <input v-model="newSleepEntry.quality" type="number" id="quality" min="1" max="10" required />
+
+      <button v-if="!editEntry" type="submit">Add Sleep Entry</button>
+      <button v-else type="submit">Save</button>
+      <button v-if="editEntry" @click="cancelEdit">Cancel</button>
+    </form>
+
+    <ul class="sleep-display-section">
+      <li v-for="(entry, index) in sleepEntries" :key="index" class="sleep-item">
+        <strong>Date:</strong> {{ entry.date }},
+        <strong>Start Time:</strong> {{ entry.startTime }},
+        <strong>Wake Time:</strong> {{ entry.wakeTime }},
+        <strong>Quality:</strong> {{ entry.quality }}
+        <strong>Duration:</strong> {{ calculateDuration(entry.startTime, entry.wakeTime) }}
+        <button class="edit-btn" @click="editSleepEntry(index)">Edit</button>
+        <button class="delete-btn" @click="deleteSleepEntry(index)">Delete</button>
+      </li>
+    </ul>
 </template>
 
 <script>
