ab8fd2153c640358f655db1dcabdc27aac05fbe1
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-22 13:46:49 +0100

    Update MealTracker.vue
    Složio CRUD za obroke i statistiku...vjerojatno ću dodati još grafikone i filter

--- src/components/MealTracker.vue
+++ src/components/MealTracker.vue
@@ -15,12 +15,128 @@
       </nav>
     </div>
     <hr>
+    <form v-if="editIndex === null" @submit.prevent="addMeal" class="meal-input-section">
+      <h3>Meal Info</h3>
+      <label for="mealName">Meal Name:</label>
+      <input v-model="newMeal.name" type="text" id="mealName" required />
+
+      <label for="mealIngredients">Ingredients:</label>
+      <textarea v-model="newMeal.ingredients" id="mealIngredients" rows="4" required></textarea>
+
+      <label for="mealCalories">Calories:</label>
+      <input v-model="newMeal.calories" type="number" id="mealCalories" required />
+
+      <button type="submit">Add Meal</button>
+    </form>
+
+    <form v-else-if="editIndex !== null" @submit.prevent="saveEdit" class="meal-input-section">
+      <label for="editMealName">Meal Name:</label>
+      <input v-model="editedMeal.name" type="text" id="editMealName" required />
+
+      <label for="editMealIngredients">Ingredients:</label>
+      <textarea v-model="editedMeal.ingredients" id="editMealIngredients" rows="4" required></textarea>
+
+      <label for="editMealCalories">Calories:</label>
+      <input v-model="editedMeal.calories" type="number" id="editMealCalories" required />
+
+      <button type="submit">Save</button>
+      <button @click="cancelEdit">Cancel</button>
+    </form>
+
+    <ul class="meal-display-section">
+      <li v-for="(meal, index) in filteredMealsByIngredient" :key="index" class="meal-item">
+        <strong>Meal Name:</strong> {{ meal.name }}<br>
+        <strong>Ingredients:</strong> {{ meal.ingredients }}<br>
+        <strong>Calories:</strong> {{ meal.calories }} kcal<br>
+        <button class="edit-btn" @click="openEditDialog(index)">Edit</button>
+        <button class="delete-btn" @click="confirmDeleteMeal(index)">Delete</button>
+      </li>
+    </ul>
+
+    <div class="statistics">
+      <strong>Total Calories:</strong> {{ totalCalories }} kcal<br>
+      <strong>Total Meals:</strong> {{ totalMeals }}<br>
+      <strong>Average Calories per Meal:</strong> {{ averageCaloriesPerMeal }} kcal
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-   
+  data() {
+    return {
+      newMeal: {
+        name: "",
+        ingredients: "",
+        calories: 0,
+      },
+      meals: [],
+      editIndex: null,
+      editedMeal: {
+        name: "",
+        ingredients: "",
+        calories: 0,
+      },
+  },
+  computed: {
+    totalCalories() {
+      return this.meals.reduce((total, meal) => total + meal.calories, 0);
+    },
+    totalMeals() {
+      return this.meals.length;
+    },
+    averageCaloriesPerMeal() {
+      if (this.totalMeals === 0) {
+        return 0;
+      }
+      return (this.totalCalories / this.totalMeals).toFixed(2);
+    },
+  },
+  methods: {
+    addMeal() {
+      this.meals.push({ ...this.newMeal });
+      this.resetForm();
+    },
+    openEditDialog(index) {
+      this.editIndex = index;
+      this.editedMeal = { ...this.meals[index] };
+    },
+    saveEdit() {
+      this.meals[this.editIndex] = { ...this.editedMeal };
+      this.editIndex = null;
+      this.editedMeal = {
+        name: "",
+        ingredients: "",
+        calories: 0,
+      };
+    },
+    confirmDeleteMeal(index) {
+      const isConfirmed = window.confirm("Do you really want to delete this meal?");
+      if (isConfirmed) {
+        this.deleteMeal(index);
+      }
+    },
+    deleteMeal(index) {
+      this.meals.splice(index, 1);
+      this.updatePieChart();
+    },
+    cancelEdit() {
+      this.editIndex = null;
+      this.editedMeal = {
+        name: "",
+        ingredients: "",
+        calories: 0,
+      };
+    },
+    resetForm() {
+      this.newMeal = {
+        name: "",
+        ingredients: "",
+        calories: 0,
+      };
+    },
+
+    },
     goBackHome() {
       this.$router.push({ path: '/' });
     },
@@ -47,5 +163,7 @@ export default {
 </script>
 
 <style scoped>
+/* CSS styles for meal tracker component */
+
 
 </style>
