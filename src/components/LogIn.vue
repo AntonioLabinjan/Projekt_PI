8d0a9fbb86cd28b542162b40ea59151382a28e8b
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-03-27 20:30:40 +0100

    Update LogIn.vue
    Dodao dark mode

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-container">
+  <div class="login-container" :class="{ 'dark-mode': darkMode }">
     <h2>Login</h2>
     <form @submit.prevent="login" class="login-form">
       <div class="form-group">
@@ -12,11 +12,11 @@
       </div>
       <div class="button-group">
         <button type="submit" class="btn btn-primary">Login</button>
-        <h3>Don't have an account?</h3>
         <button @click="goToSignUp" class="btn btn-secondary">Sign-up</button>
         <button @click="goBackHome" class="btn btn-secondary">Home</button>
       </div>
     </form>
+    <button @click="toggleDarkMode" class="btn btn-dark">{{ darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</button>
   </div>
 </template>
 
@@ -25,12 +25,13 @@ export default {
   data() {
     return {
       loginUsername: '',
-      loginPassword: ''
-    }
+      loginPassword: '',
+      darkMode: false
+    };
   },
   methods: {
     login() {
-      // Implementacija logike za prijavu će biti dodana kasnije (firebase :) )
+      // Implementacija logike za prijavu će biti dodana kasnije
       console.log('Prijavljivanje s korisničkim imenom:', this.loginUsername, 'i lozinkom:', this.loginPassword);
     },
     goToSignUp() {
@@ -39,9 +40,21 @@ export default {
     goBackHome() {
       this.$router.push({ path: '/' });
     },
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+      if (this.darkMode) {
+        document.body.classList.add('dark-mode');
+      } else {
+        document.body.classList.remove('dark-mode');
+      }
+    }
   }
-}
+};
 </script>
 
 <style scoped>
+.dark-mode {
+  background-color: #333;
+  color: #fff;
+}
 </style>
