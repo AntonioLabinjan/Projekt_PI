4ba67fafb203f6fef1df5bc8b5d393181c11a7dc
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-03 18:13:20 +0200

    Update MusicPlayer.vue
    Sređeno rutiranje

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -2,6 +2,17 @@
   <div class="music-player-container" :class="{ 'dark-mode': isDarkMode }">
     <div class="header">
       <h1>Music Player</h1>
+      <nav>
+      <ul class="navbar">
+        <li><button @click="goToImageGallery">Image Gallery</button></li>
+        <li><button @click="goToVueTrainer">Training App</button></li>
+        <li><button @click="goToMealTracker">Meal Tracker</button></li>
+        <li><button @click="goToSleepTracker">Sleep Tracker</button></li>
+        <li><button @click="goToWaterIntake">Water Intake Tracker</button></li>
+        <li><button @click="goToBMI">BMI Calculator</button></li>
+        <li><button @click="goToStreak">Streak Tracker</button></li>
+      </ul>
+    </nav>
       <button @click="toggleDarkMode" class="dark-mode-button">{{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}</button>
     </div>
     <div class="search-bar">
@@ -96,7 +107,40 @@ export default {
     },
     toggleDarkMode() {
       this.isDarkMode = !this.isDarkMode;
-    }
+    },
+    goToImageGallery() {
+      this.$router.push({ path: '/image-gallery' });
+    },
+    goToVueTrainer() {
+      this.$router.push({ path: '/vue-trainer' });
+    },
+    goToMealTracker() {
+      this.$router.push({ path: '/meal-tracker' });
+    },
+    goToLogIn() {
+      this.$router.push({ path: '/login' });
+    },
+    goToSignUp() {
+      this.$router.push({ path: '/sign-up' });
+    },
+    goToSleepTracker() {
+      this.$router.push({ path: '/sleep-tracker' });
+    },
+    goToWaterIntake() {
+      this.$router.push({ path: '/water-intake' });
+    },
+    goToBMI() {
+      this.$router.push({ path: '/BMI-calculator' });
+    },
+    goToAbout() {
+      this.$router.push({ path: '/about' });
+    },
+    goToStreak() {
+      this.$router.push({ path: '/streak' });
+    },
+    goToScanner() {
+      this.$router.push({ path: '/qr-scanner'});
+    },
   }
 };
 </script>
