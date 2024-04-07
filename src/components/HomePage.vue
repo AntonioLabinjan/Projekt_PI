a68da0de8306fe970eb2b9fe2c6f5d23ace347bc
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-07 23:17:36 +0200

    Update HomePage.vue
    Dodao opciju da nam aplikacija "pročita" motivacijski citat

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -29,9 +29,9 @@
     <div><button @click="goToScanner">Go to Scanner</button></div>
     <div class="motivational-quote">
       <p>{{ motivationalQuote }}</p>
+      <button @click="readMotivationalQuote">Read The Quote</button>
     </div>
 
-    <!-- Display current caloric balance -->
     <div class="caloric-balance">
       <p>Current caloric balance: {{ currentCaloricBalance }}</p>
     </div>
@@ -48,30 +48,30 @@ export default {
     return {
       darkMode: false,
       motivationalQuotes: [
-  "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-  "The only way to do great work is to love what you do. - Steve Jobs",
-  "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
-  "Believe you can and you're halfway there. - Theodore Roosevelt",
-  "It does not matter how slowly you go as long as you do not stop. - Confucius",
-  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-  "In the middle of every difficulty lies opportunity. - Albert Einstein",
-  "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-  "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-  "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
-  "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
-  "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
-  "Success is walking from failure to failure with no loss of enthusiasm. - Winston Churchill",
-  "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-  "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-  "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
-  "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
-  "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
-  "The best revenge is massive success. - Frank Sinatra",
-  "Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll",
-  "The harder I work, the luckier I get. - Samuel Goldwyn",
-  "The only way to achieve the impossible is to believe it is possible. - Charles Kingsleigh (Alice in Wonderland)",
-],
-
+        "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+        "The only way to do great work is to love what you do. - Steve Jobs",
+        "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+        "Believe you can and you're halfway there. - Theodore Roosevelt",
+        "It does not matter how slowly you go as long as you do not stop. - Confucius",
+        "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+        "In the middle of every difficulty lies opportunity. - Albert Einstein",
+        "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+        "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+        "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
+        "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
+        "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+        "Success is walking from failure to failure with no loss of enthusiasm. - Winston Churchill",
+        "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+        "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+        "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
+        "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
+        "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+        "The best revenge is massive success. - Frank Sinatra",
+        "Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll",
+        "The harder I work, the luckier I get. - Samuel Goldwyn",
+        "The only way to achieve the impossible is to believe it is possible. - Charles Kingsleigh (Alice in Wonderland)",
+        "Never gonna give you up, never gonna let you down, never gonna run around and desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you. - Rick Astley"
+      ],
       motivationalQuote: ""
     };
   },
@@ -122,6 +122,14 @@ export default {
     selectRandomQuote() {
       const randomIndex = Math.floor(Math.random() * this.motivationalQuotes.length);
       this.motivationalQuote = this.motivationalQuotes[randomIndex];
+    },
+    readMotivationalQuote() {
+      if ('speechSynthesis' in window) {
+        const utterance = new SpeechSynthesisUtterance(this.motivationalQuote);
+        window.speechSynthesis.speak(utterance);
+      } else {
+        alert('Your browser does not support text-to-speech.');
+      }
     }
   },
   mounted() {
