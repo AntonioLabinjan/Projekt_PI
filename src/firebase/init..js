60af4f5b2a2680928aa748701de6000975fa7a12
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-11 15:20:49 +0200

    Update init..js
    Ne uspijevam se povezati na firebase kako treba, pa sam ovdje probao ubaciti try-catch block, ali ništa ne lovi...aplikacija mi ne želi prepoznati firebase

--- src/firebase/init..js
+++ src/firebase/init..js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +13,14 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore()
-export default db
+let app;
+let db;
+
+try {
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+} catch (error) {
+  console.error("Error initializing Firebase app:", error);
+}
+
+export default db;
