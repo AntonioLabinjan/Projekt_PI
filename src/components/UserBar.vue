9d5f5b102e093ff94a99b8746528579f61fc2ed2
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-23 12:55:44 +0200

    Update UserBar.vue
    Dodao malo css-a na user-bar

--- src/components/UserBar.vue
+++ src/components/UserBar.vue
@@ -1,48 +1,70 @@
 <template>
-    <div class="user-info" v-if="currentUser">
-      <p><b>Trenutno ulogirani korisnik:</b> {{ currentUser.email }}</p>
-      <li><button @click="logout">Odlogiraj se</button></li>
-    </div>
-  </template>
-  
-  <script>
-  import { onAuthStateChanged, signOut } from 'firebase/auth';
-  import { auth } from '@/firebase';
-  
-  export default {
-    data() {
-      return {
-        currentUser: null,
-      };
+  <div class="user-info" v-if="currentUser">
+    <p><b>Trenutno ulogirani korisnik:</b> {{ currentUser.email }}</p>
+    <li><button @click="logout">Odlogiraj se</button></li>
+  </div>
+</template>
+
+<script>
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '@/firebase';
+
+export default {
+  data() {
+    return {
+      currentUser: null,
+    };
+  },
+  methods: {
+    async logout() {
+      try {
+        await signOut(auth);
+        window.alert("Uspješno ste se odlogirali.");
+        this.$router.push('/');
+      } catch (error) {
+        console.error('Greška prilikom odlogiranja:', error);
+        window.alert("Došlo je do pogreške prilikom odlogiranja.");
+      }
     },
-    methods: {
-      async logout() {
-        try {
-          await signOut(auth);
-          window.alert("Uspješno ste se odlogirali.");
-          this.$router.push('/');
-        } catch (error) {
-          console.error('Greška prilikom odlogiranja:', error);
-          window.alert("Došlo je do pogreške prilikom odlogiranja.");
-        }
-      },
-    },
-    mounted() {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          this.currentUser = user;
-        } else {
-          this.currentUser = null;
-        }
-      });
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .user-info {
-    margin-top: 20px;
-    text-align: center;
-  }
-  </style>
-  
+  },
+  mounted() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.currentUser = user;
+      } else {
+        this.currentUser = null;
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+.user-info {
+  margin-top: 20px;
+  text-align: center;
+  background-color: #58CA2B; 
+  padding: 15px; 
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+}
+
+.user-info p {
+  margin-bottom: 15px; 
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff; 
+  color: #fff; 
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease; 
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+</style>
