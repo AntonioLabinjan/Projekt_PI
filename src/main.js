d4b55c1ba71f70022a5da140f9c03c716162e2bf
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-26 11:10:34 +0200

    Update main.js
    Implementacija admina u ruter...sad to treba napraviti i u firebaseu :(

--- src/main.js
+++ src/main.js
@@ -13,11 +13,15 @@ import WaterIntake from './components/WaterIntake.vue';
 import BMICalculator from './components/BMICalculator.vue';
 import AboutPage from './components/AboutPage.vue';
 import DefaultExercises from './components/DefaultExercises.vue';
+import DefaultExercisesNotAdmin from './components/DefaultExercisesNotAdmin.vue';
+import DefaultMealsNotAdmin from './components/DefaultMealsNotAdmin.vue';
 import DefaultMeals from './components/DefaultMeals.vue';
 import StreakCheck from './components/StreakCheck.vue';
 import QrScanner from './components/QrScanner.vue';
 import notificationMaker from './components/notificationMaker.vue';
+import UserBar from './components/UserBar.vue';
 import MusicPlayer from './components/MusicPlayer.vue';
+
 import { getAuth } from 'firebase/auth';
 import { onAuthStateChanged } from 'firebase/auth';
 let isUserLoggedIn = false;
@@ -32,6 +36,28 @@ function isLoggedIn() {
   return isUserLoggedIn;
 }
 
+function requiresAdmin(to, from, next) {
+  const currentUser = firebase.auth().currentUser;
+
+  if (currentUser) {
+    firebase.firestore().collection('users').doc(currentUser.uid).get()
+      .then(doc => {
+        if (doc.exists && doc.data().isAdmin) {
+          next();
+        } else {
+          next({ path: '/' }); // tu bin moga napravit custom not admin error komponentu
+        }
+      })
+      .catch(error => {
+        console.error("Error getting user document:", error);
+        next({ path: '/' }); 
+      });
+  } else {
+    next({ path: '/' }); 
+  }
+}
+
+
 const routes = [
   { path: '/', component: HomePage },
   { path: '/image-gallery', component: ImageGallery, meta: { requiresAuth: true } },
@@ -44,8 +70,10 @@ const routes = [
   { path: '/BMI-calculator', component: BMICalculator, meta: { requiresAuth: true } },
   { path: '/about', component: AboutPage },
   { path: '/streak', component: StreakCheck, meta: { requiresAuth: true } },
-  { path: '/default-exercises', component: DefaultExercises },
-  { path: '/default-meals', component: DefaultMeals },
+  { path: '/default-exercises', component: DefaultExercises, meta: {requiresAdmin: true} },
+  { path: '/default-meals', component: DefaultMeals, meta: {requiresAdmin: true} },
+  { path: '/NA-exercises', component: DefaultExercisesNotAdmin},
+  { path: '/NA-meals', component: DefaultMealsNotAdmin},
   { path: '/qr-scanner', component: QrScanner, meta: { requiresAuth: true } },
   { path: '/notification-maker', component: notificationMaker },
   { path: '/music', component: MusicPlayer, meta: { requiresAuth: true } },
@@ -64,7 +92,7 @@ router.beforeEach((to, from, next) => {
     next('/login');  // Ako nismo ulogirani, šalje nas na login
   } else if (!isRequiresAuth && isLoggedIn() && to.path === '/login') {
     window.alert("već ste ulogirani, ne možete opet") // ne pusti nas da se opet ulogiramo ako smo ulogirani i šalje nas na homepage
-    next('/');  
+    next('/');   // forši ću napravit neki custom error page
   } else {
     next();  // ako smo ulogirani i klikćemo na neku rutu koja ni login, onda je okej i šalje nas na traženu rutu
   }
@@ -88,6 +116,9 @@ app.component('StreakCheck', StreakCheck);
 app.component('QrScanner', QrScanner);
 app.component('notificationMaker', notificationMaker);
 app.component('MusicPlayer', MusicPlayer);
+app.component('UserBar', UserBar)
+app.component('DefaultExercisesNotAdmin', DefaultExercisesNotAdmin)
+app.component('DefaultMealsNotAdmin', DefaultMealsNotAdmin);
 app.use(router);
 app.use(store);
 
