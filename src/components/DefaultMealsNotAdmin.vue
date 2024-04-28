30d95fe419a103da7de5312a640c1be31b274c4e
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-28 11:33:20 +0200

    Update DefaultMealsNotAdmin.vue
    Maknuo sam ove hardkodirane obroke, i sad se vide samo oni koje admin doda

--- src/components/DefaultMealsNotAdmin.vue
+++ src/components/DefaultMealsNotAdmin.vue
@@ -9,52 +9,116 @@
         <p><strong>Kalorije:</strong> {{ meal.calories }}</p>
         <p><strong>Opis:</strong> {{ meal.description }}</p>
         <img :src="meal.image" alt="Primjer obroka">
-        <button @click="editMeal(index)" class="edit">Uredi</button>
-        <button @click="deleteMeal(index)" class="delete">Obriši</button>
         <hr>
       </div> 
-      <!-- Button to go back to the about page -->
       <button @click="goBack">Go Back</button>
+      <button @click="editAsAdmin">Edit meals as admin</button>
       <button @click="toggleDarkMode" class="btn btn-dark">{{ darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</button>
     </div>
   </template>
   
   <script>
-  export default {
-    data() {
-      return {
-        meals: [
-    { name: 'Piletina s rižom', mainIngredient: 'Piletina', calories: 350, description: 'Jednostavan i ukusan obrok s piletinom i rižom.', image: 'https://th.bing.com/th/id/R.8fd11496f3d45066791fed39417a237e?rik=06xBu9A0FTilJg&pid=ImgRaw&r=0' },
-    { name: 'Povrtni curry', mainIngredient: 'Povrće', calories: 300, description: 'Zdrav i začinjen curry s raznovrsnim povrćem.', image: 'https://th.bing.com/th/id/OIP.q9NcYeZqQwjUsmUBe8pvowHaLE?w=115&h=192&c=7&r=0&o=5&pid=1.7' },
-    { name: 'Tjestenina s pestom', mainIngredient: 'Tjestenina', calories: 400, description: 'Klasični talijanski obrok s tjesteninom i umakom od pesta.', image: 'https://th.bing.com/th/id/OIP.j2QTs_0S8WS2tLjOAcs7gQHaLG?w=189&h=283&c=7&r=0&o=5&pid=1.7' },
-    { name: 'Losos sa šparogama', mainIngredient: 'Losos', calories: 450, description: 'Bogati obrok s lososom i svježim šparogama.', image: 'https://th.bing.com/th/id/OIP.AMCHHAb4FN5rM-LwQMtg2AHaHa?w=189&h=189&c=7&r=0&o=5&pid=1.7' },
-    { name: 'Salata s tunom', mainIngredient: 'Tuna', calories: 250, description: 'Lagani obrok s tunom i svježim povrćem.', image: 'https://th.bing.com/th/id/OIP.-RebUdOn42ST6UgFA1Y6HAHaE9?w=189&h=126&c=7&r=0&o=5&pid=1.7' },
-    { name: 'Pileći sendvič', mainIngredient: 'Piletina', calories: 300, description: 'Klasični pileći sendvič sa salatom i majonezom.', image: 'https://th.bing.com/th/id/R.888daf7be1ae8858ba0608306bf4c0f7?rik=qr6wIL%2bR7aAR4w&pid=ImgRaw&r=0' },
-    { name: 'Pizza s rajčicom i mozzarellom', mainIngredient: 'Rajčica', calories: 500, description: 'Sočna pizza s tankim tijestom, rajčicom i svježim mozzarellom.', image: 'https://th.bing.com/th/id/R.a53ddb06e150e57cd84d4750b25827d8?rik=JRPEge%2bybzOWlQ&pid=ImgRaw&r=0' },
-    { name: 'Quinoa salata', mainIngredient: 'Quinoa', calories: 350, description: 'Zdrava quinoa salata s povrćem i avokadom, začinjena limunovim sokom.', image: 'https://th.bing.com/th/id/OIP.Sf8p0wwoTX_Va_ixfqqbwwHaLJ?w=189&h=285&c=7&r=0&o=5&pid=1.7' },
-    { name: 'Pileća juha', mainIngredient: 'Piletina', calories: 200, description: 'Topla i umirujuća pileća juha s povrćem.', image: 'https://th.bing.com/th/id/OIP.OOgcP-cdwn7AEH9N3Gly5gHaLH?w=189&h=284&c=7&r=0&o=5&pid=1.7' }
-  ],
-  
-        darkMode: false,
-      };
-    },
-    methods: {
-      
-      goBack() {
-        this.$router.push({ path: '/about' });
+
+import { db } from '@/firebase';
+import { collection, getDocs, addDoc, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+
+export default {
+  data() {
+    return {
+      meals: [],
+      newMeal: {
+        name: '',
+        mainIngredient: '',
+        calories: 0,
+        description: '',
+        image: ''
       },
-      toggleDarkMode() {
-        this.darkMode = !this.darkMode;
-        if(this.darkMode){
-          document.body.classList.add('dark-mode');
-        } else {
-          document.body.classList.remove('dark-mode');
-        }
-        }
-      }
+      editIndex: null,
+      editMode: false,
+      darkMode: false,
     };
-  
-  </script>
+  },
+  created() {
+    this.fetchMeals();
+  },
+  methods: {
+    
+    goBack(){
+      this.$router.push('/about');
+    },
+    editAsAdmin(){
+      this.$router.push('/default-meals');
+    },
+    async fetchMeals() {
+      const querySnapshot = await getDocs(collection(db, "mealSuggestions"));
+      this.meals = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    },
+    async addMeal() {
+      await addDoc(collection(db, "mealSuggestions"), {
+        name: this.newMeal.name,
+        mainIngredient: this.newMeal.mainIngredient,
+        calories: this.newMeal.calories,
+        description: this.newMeal.description,
+        url: this.newMeal.image
+      });
+      this.fetchMeals();
+      this.resetForm();
+    },
+    },
+    editMeal(index) {
+      this.editIndex = index;
+      this.newMeal = { ...this.meals[index] };
+      this.newMeal.image = this.newMeal.url;
+      this.editMode = true;
+    },
+
+    async updateMeal() {
+      if (this.editIndex !== null) {
+        const mealId = this.meals[this.editIndex].id;
+        await updateDoc(doc(db, "mealSuggestions", mealId), {
+          name: this.newMeal.name,
+          mainIngredient: this.newMeal.mainIngredient,
+          calories: this.newMeal.calories,
+          description: this.newMeal.description,
+          url: this.newMeal.image
+        });
+        this.fetchMeals();
+        this.resetForm();
+      }
+    },
+
+    submitMeal() {
+      if (this.editMode) {
+        this.updateMeal();
+      } else {
+        this.addMeal();
+      }
+    },
+    async deleteMeal(index) {
+      const mealId = this.meals[index].id;
+      await deleteDoc(doc(db,'mealSuggestions',mealId));
+      this.fetchMeals();
+    },
+    resetForm() {
+      this.newMeal = { name: '', mainIngredient: '', calories: 0, description: '', image: '' };
+      this.editIndex = null;
+      this.editMode = false;
+    },
+    
+    cancelEdit() {
+      this.resetForm();
+    },
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+      if (this.darkMode) {
+        document.body.classList.add('dark-mode');
+      } else {
+        document.body.classList.remove('dark-mode');
+      }
+    },
+  };
+</script>
+
   
   <style scoped>
   /* Globalni stilovi */
