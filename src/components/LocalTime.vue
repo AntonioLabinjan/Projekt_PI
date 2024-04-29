e07d899c01fa9a0727b007d0c4fe5a01bf5e7d49
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-04-29 12:34:31 +0200

    Create LocalTime.vue
    Dodao komponentu za praćenje vremena

---
+++ src/components/LocalTime.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="local-time">
+      Trenutno vrijeme: {{ currentTime }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LocalTime',
+  
+    data() {
+      return {
+        currentTime: new Date().toLocaleTimeString()
+      };
+    },
+  
+    mounted() {
+      this.timer = setInterval(() => {
+        this.currentTime = new Date().toLocaleTimeString();
+      }, 1000);
+    },
+  
+    beforeUnmount() {
+      clearInterval(this.timer);
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .local-time {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  </style>
+  
