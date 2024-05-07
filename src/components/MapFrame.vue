625822c20c05f9752562b12963dc3bfb17c204d5
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-05-08 00:55:36 +0200

    Create MapFrame.vue

---
+++ src/components/MapFrame.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="map-container" style="width: 200%; height: 400px;">
+    <h4>Where can you find me?(This may or may not be correct location...I don't know)</h4>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2808.781402860673!2d13.897366276148244!3d45.25221297107129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477caf055a616649%3A0xef015611980e5d1f!2s6%2C%2052000%2C%20Beram!5e0!3m2!1sen!2shr!4v1715121806994!5m2!1sen!2shr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+</template>
+<script></script>
+<style>
+.map-container {
+  max-width: 600px;
+  height: 300px;
+  margin: auto;
+}
+</style>
