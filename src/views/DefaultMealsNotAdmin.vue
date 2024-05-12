06532057f3fc7087c924dd2a788945cc2de767db
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-05-12 11:52:17 +0200

    Update DefaultMealsNotAdmin.vue
    Dodao searchbar

--- src/views/DefaultMealsNotAdmin.vue
+++ src/views/DefaultMealsNotAdmin.vue
@@ -1,23 +1,24 @@
 <template>
-    <div class="container" :class="{'dark-mode': darkMode}">
-      <h1 class="h1" :class="{'dark-mode': darkMode}">Galerija Obroka</h1>
+  <div class="container" :class="{'dark-mode': darkMode}">
+    <h1 class="h1" :class="{'dark-mode': darkMode}">Meal Gallery</h1>
+    <input type="text" v-model="searchQuery" placeholder="Search meals..." class="search-bar">
+    <hr>
+    <!-- Prikaz filtriranih obroka -->
+    <div v-for="(meal, index) in filteredMeals" :key="index" class="meal">
+      <h2>{{ meal.name }}</h2>
+      <p><strong>Main Ingredient:</strong> {{ meal.mainIngredient }}</p>
+      <p><strong>Calories:</strong> {{ meal.calories }}</p>
+      <p><strong>Description:</strong> {{ meal.description }}</p>
+      <img :src="meal.image" alt="Example meal">
       <hr>
-      <!-- Prikaz hardkodiranih obroka -->
-      <div v-for="(meal, index) in meals" :key="index" class="meal">
-        <h2>{{ meal.name }}</h2>
-        <p><strong>Glavni sastojak:</strong> {{ meal.mainIngredient }}</p>
-        <p><strong>Kalorije:</strong> {{ meal.calories }}</p>
-        <p><strong>Opis:</strong> {{ meal.description }}</p>
-        <img :src="meal.image" alt="Primjer obroka">
-        <hr>
-      </div> 
-      <button @click="goBack">Go Back</button>
-      <button @click="editAsAdmin">Edit meals as admin</button>
-      <button @click="toggleDarkMode" class="btn btn-dark">{{ darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</button>
-    </div>
-  </template>
-  
-  <script>
+    </div> 
+    <button @click="goBack">Go Back</button>
+    <button @click="editAsAdmin">Edit meals as admin</button>
+    <button @click="toggleDarkMode" class="btn btn-dark">{{ darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</button>
+  </div>
+</template>
+
+<script>
 
 import { db } from '@/firebase';
 import { collection, getDocs, addDoc, doc, deleteDoc, updateDoc } from 'firebase/firestore';
@@ -36,19 +37,20 @@ export default {
       editIndex: null,
       editMode: false,
       darkMode: false,
+      searchQuery: ''
     };
   },
   created() {
     this.fetchMeals();
   },
+  computed: {
+    filteredMeals() {
+      return this.meals.filter(meal =>
+        meal.name.toLowerCase().includes(this.searchQuery.toLowerCase()) || meal.mainIngredient.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  },
   methods: {
-    
-    goBack(){
-      this.$router.push('/about');
-    },
-    editAsAdmin(){ // obrisa san fromPage
-      this.$router.push({path:'/admin-login', query: { redirect: '/default-meals'}});
-    },
     async fetchMeals() {
       const querySnapshot = await getDocs(collection(db, "mealSuggestions"));
       this.meals = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
@@ -64,14 +66,12 @@ export default {
       this.fetchMeals();
       this.resetForm();
     },
-    },
     editMeal(index) {
       this.editIndex = index;
       this.newMeal = { ...this.meals[index] };
       this.newMeal.image = this.newMeal.url;
       this.editMode = true;
     },
-
     async updateMeal() {
       if (this.editIndex !== null) {
         const mealId = this.meals[this.editIndex].id;
@@ -86,7 +86,6 @@ export default {
         this.resetForm();
       }
     },
-
     submitMeal() {
       if (this.editMode) {
         this.updateMeal();
@@ -104,115 +103,125 @@ export default {
       this.editIndex = null;
       this.editMode = false;
     },
-    
     cancelEdit() {
       this.resetForm();
     },
     toggleDarkMode() {
       this.darkMode = !this.darkMode;
+      const app = document.getElementById('app');
       if (this.darkMode) {
-        document.body.classList.add('dark-mode');
+        app.classList.add('dark-mode');
       } else {
-        document.body.classList.remove('dark-mode');
+        app.classList.remove('dark-mode');
       }
     },
-  };
+    editAsAdmin(){ // obrisa saan fromPage
+      this.$router.push({path:'/admin-login', query: { redirect: '/default-meals'}});
+    },
+    goBack(){
+      this.$router.push('/about');
+    },
+  }
+};
 </script>
 
-  
-  <style scoped>
-  body {
-    font-family: Arial, sans-serif;
-    background-color: #f2f2f2;
-    margin: 0;
-    padding: 0;
-  }
-  
-  #app {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-  
-  h1 {
-    text-align: center;
-    color: #333;
-  }
-  
-  .meal {
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    margin-bottom: 20px;
-    padding: 10px;
-    background-color: #fff;
-  }
-  
-  .meal img {
-    max-width: 100%; 
-    height: auto; 
-    margin-top: 10px;
-    object-fit: contain; 
-    max-height: 200px; 
-  }
-  
-  button {
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    padding: 5px 10px;
-    cursor: pointer;
-    border-radius: 3px;
-    margin-left: 5px;
-  }
-  
-  button.delete {
-    background-color: #dc3545;
-  }
-  
-  button.edit {
-    background-color: #28a745;
-  }
-  
-  
-  form {
-    margin-top: 20px;
-    background-color: #f8f8f8;
-    padding: 20px;
-    border-radius: 5px;
-  }
-  
-  form label {
-    display: block;
-    margin-bottom: 5px;
-  }
-  
-  form input[type="text"],
-  form input[type="number"],
-  form textarea {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    box-sizing: border-box;
-  }
-  
-  form button {
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    cursor: pointer;
-    border-radius: 3px;
-  }
-  .container.dark-mode {
-    background-color: #000;
-    color: #fff;
-  }
-  
-  .h1.dark-mode{
-    background-color: #000;
-    color: #fff;
-  }
-  </style>
-  
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+.meal {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #fff;
+}
+
+.meal img {
+  max-width: 100%; 
+  height: auto; 
+  margin-top: 10px;
+  object-fit: contain; 
+  max-height: 200px; 
+}
+
+button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  border-radius: 3px;
+  margin-left: 5px;
+}
+
+button.delete {
+  background-color: #dc3545;
+}
+
+button.edit {
+  background-color: #28a745;
+}
+
+
+form {
+  margin-top: 20px;
+  background-color: #f8f8f8;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+form label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+form input[type="text"],
+form input[type="number"],
+form textarea {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+}
+
+form button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 3px;
+}
+
+.dark-mode {
+  background-color: #000;
+  color: #fff;
+}
+
+.search-bar {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+}
+</style>
