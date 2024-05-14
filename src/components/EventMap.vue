a97b3aefcdb180f72eb344801ec172ac370e846e
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-05-14 18:49:57 +0200

    Update EventMap.vue
    Spojio se na mapbox umisto google mapsa

--- src/components/EventMap.vue
+++ src/components/EventMap.vue
@@ -1,15 +1,14 @@
 <template>
-    <div class="map-container" style="width: 200%; height: 400px;">
-      <h4>Search for events on map (click for full screen)</h4>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d89005.01814168721!2d15.971123200000001!3d45.8031104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1shr!2shr!4v1715160224270!5m2!1shr!2shr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  </template>
-  <script></script>
-  <style>
-  .map-container {
-    max-width: 600px;
-    height: 300px;
-    margin: auto;
-  }
-  </style>
-  
+  <div class="map-container" style="width: 200%; height: 400px;">
+    <h4>Search for events on map (click for full screen)</h4>
+    <iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/123456-a/clw6kg3ba02d001o0cxjrdlfm.html?title=false&access_token=pk.eyJ1IjoiMTIzNDU2LWEiLCJhIjoiY2x3NmtiaGNjMXB6MTJpbXVsNGUyZWd0OCJ9.7HlKhRuY3b8pQqRzXDvjtA&zoomwheel=false#5.25/43.454/12.261" title="Untitled" style="border:none;"></iframe>
+  </div>
+</template>
+<script></script>
+<style>
+.map-container {
+  max-width: 600px;
+  height: 300px;
+  margin: auto;
+}
+</style>
